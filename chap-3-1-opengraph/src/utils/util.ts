9fbb42c879c95a8b7259753b33f79b1187806f29
semonec <semonec@gmail.com>
2023-09-10 18:17:39 +0900

    feat: preparing deployment to vercel

--- chap-3-1-opengraph/src/utils/util.ts
+++ chap-3-1-opengraph/src/utils/util.ts
@@ -1,5 +1,5 @@
 export const getCurrentUrl = (path: string) => {
-    const host = process.env.NEXT_PUBLIC_HOST
+    // deployed to vercel / else case / localhost
+    const host = process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.NEXT_PUBLIC_HOST ?? 'http://localhost:3000'
     return `${host}${path}`
-
 }
